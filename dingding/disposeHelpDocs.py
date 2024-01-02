8e3a85d84e7f8f8b93d448f63eab45c53ba85e4e
liuhangyu <2323165746@qq.com>
2024-01-02 17:31:33 +0800

    实现机器人回复帮助文档功能

--- dingding/disposeHelpDocs.py
+++ dingding/disposeHelpDocs.py
@@ -14,7 +14,7 @@ class DisposeHelpDocs:
         self.text: str = ""
         self.doc_type: 0
         self.error = 0
-        self.result = "aasddda"
+        self.result = "aasdssdda"
 
         # 查询全部记录
         if self.message == "" or "帮助文档" in self.message:
